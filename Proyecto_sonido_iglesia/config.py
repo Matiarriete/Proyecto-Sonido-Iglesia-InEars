09b726423b8b58a06b2d87a3822017b34d3d2b04
matiarriete <matiasarriete@hotmail.com>
2026-01-16 23:47:29 +0100

    Implementacion de mixer connection funcional para MR18

--- Proyecto_sonido_iglesia/config.py
+++ Proyecto_sonido_iglesia/config.py
@@ -2,7 +2,6 @@ import os
 
 # --- Configuración de la Consola X32/X18 ---
 # Si la variable de entorno X32_IP no existe, la aplicación fallará al inicio (None)
-X32_IP: str | None = os.environ.get("X32_IP") 
-X32_PORT: int | None = 10023 # El puerto OSC es estándar
+MIXER_IP: str
 
 DATABASE_URL = "sqlite:///./app.db" 
